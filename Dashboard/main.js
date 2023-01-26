803662d90329620925f7bd4d555b6c1c982b93ce
janay <janayby@gmail.com>
2023-01-26 11:15:57 +0200

    start error handling and add default bg

--- Dashboard/main.js
+++ Dashboard/main.js
@@ -1,7 +1,11 @@
-fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=[Access-key]")
+fetch("https://ap.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=[Access-key]")
   .then(res => res.json())
   .then(data => {
-    const output = data.urls.full
-    console.log(output)
-    document.body.style.backgroundImage = `url(${output})`
+    // const output = data.urls.full
+    // console.log(output)
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.getElementById('author-name').textContent = `Photo By: ${data.user.name}`
+  })
+  .catch(err => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1920)`
   })
