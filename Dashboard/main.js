105eaf9202393df1e798a814d988d841f3421c9b
janay <janayby@gmail.com>
2023-01-30 19:40:00 +0200

    add time with ampm

--- Dashboard/main.js
+++ Dashboard/main.js
@@ -1,8 +1,10 @@
-fetch("https://ap.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=[Access-key]")
+// get image from unsplash
+fetch("https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=UIY5IqxOXXs_MCEKt7IRiQk9u7bLWrgxM_xHN4iBDMU")
   .then(res => {
     return res.json()
   })
   .then(data => {
+    console.log(data)
     document.body.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById('author-name').textContent = `Photo By: ${data.user.name}`
   })
@@ -11,7 +13,7 @@ fetch("https://ap.unsplash.com/photos/random?orientation=landscape&query=nature&
     document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1920)"
   })
 
-
+// get crypto info from coingecko
   fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res => {
       if (!res.ok) {
@@ -36,4 +38,18 @@ fetch("https://ap.unsplash.com/photos/random?orientation=landscape&query=nature&
 
     })
     .catch(err => console.error(err))
-
+    
+    // add time
+    function formatAMPM(date) {
+      let hours = date.getHours()
+      let minutes = date.getMinutes()
+      const ampm = hours >= 12 ? 'pm' : 'am'
+      hours = hours % 12
+      hours = hours ? hours : 12 // the hour 0 should be 12
+      minutes = minutes < 10 ? '0' + minutes : minutes
+      const strTime = `${hours}:${minutes} ${ampm}`
+      return strTime
+    }
+    const time = formatAMPM(new Date)
+    console.log(time)
+    document.getElementById("time").innerHTML = `<span>${time}</span>`
